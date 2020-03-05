57af0e4ba4ac58bf73b0fa0108dde61e25f4aeae
russel febrishky <rfgalahad23@gmail.com>
2020-03-05 15:40:01 +0800

    Initial Commit

---
+++ app/src/main/java/com/reverse/canvastech/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.reverse.canvastech
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
